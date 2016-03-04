d090dd547e95495eb3810aa384b1ce706940dd0e
reyna lidia chambilla chura <reynachambilla@gmail.com>
2016-03-04 11:00:10 -0500

    pagina

---
+++ index4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="estilos4.css">
+</head>
+<body>
+	<div>
+		<nav>
+			<ul class="menu-list">
+				<li class="menu-item"><a href="" class="menu-link">INICIO</a></li>
+				<li class="menu-item"><a href="" class="menu-link">GALERIA</a></li>
+				<li class="menu-item"><a href="" class="menu-link">BLOG</a></li>
+				<li class="menu-item"><a href="" class="menu-link">ANUNCIOS</a></li>
+				<li class="menu-item"><a href="" class="menu-link">CONTÁCTOS</a></li>
+			</ul>
+		</nav>
+		<div>
+			<img class="img" src="12C4813AC7.jpg" alt="mi imagen" height="500" width="1330">
+			<img class="img" src="lidia.gif" alt="mi imagen" height="225" width="322">
+			<img class="img" src="Captura.JPG" alt="mi imagen" height="225" width="322">
+			<img class="img" src="pegar.JPG" alt="mi imagen" height="225" width="322">
+			<img class="img" src="paisajes-hermosos.jpg" alt="mi imagen" height="225" width="322">
+	</div>
+</body>
+</html>
